c27ccca5334a19afaf002cefa0b71a5e926d345c
David Pech <pech@raynet.cz>
2013-03-10 12:13:46 +0100

    + basic http server

---
+++ src/main/scala/cz/pechdavid/mycelium/extension/http/WebServerModule.scala
@@ -0,0 +1,29 @@
+package cz.pechdavid.mycelium.extension.http
+
+import cz.pechdavid.mycelium.core.module.{StopModule, StartModule, WorkerModule}
+import spray.routing.SimpleRoutingApp
+import net.liftweb.json.JsonAST.JValue
+import spray.routing.Route
+import akka.actor.PoisonPill
+
+/**
+ * Created: 3/10/13 11:19 AM
+ */
+class WebServerModule(name: String, interface: String, port: Int)(route: Route) extends WorkerModule(name) with SimpleRoutingApp {
+  def extract(parsedPayload: JValue) = {
+    parsedPayload
+  }
+
+  def handle = {
+    case StartModule =>
+      startServer(interface, port)(route)
+
+    case StopModule =>
+      context.child("http-server") match {
+        case Some(act) =>
+          act ! PoisonPill
+        case None =>
+          // pass
+      }
+  }
+}
--- src/main/scala/cz/pechdavid/webweaver/Launcher.scala
+++ src/main/scala/cz/pechdavid/webweaver/Launcher.scala
@@ -1,8 +1,33 @@
 package cz.pechdavid.webweaver
 
+import crawler.WebWeaver
+import cz.pechdavid.mycelium.core.module.ModuleSpec
+import cz.pechdavid.mycelium.extension.http.WebServerModule
+import spray.routing.{Directives, Route}
+import akka.actor.Props
+
 /**
  * Created: 2/22/13 5:46 PM
  */
-object Launcher extends App {
+object Launcher extends App with Directives {
+
+  val route: Route = {
+    path("") {
+      get {
+        complete {
+          "Hello!"
+        }
+      }
+    }
+  }
+
+
+  val httpServer = (ms: ModuleSpec) => {
+    Props(new WebServerModule(ms.name, "localhost", 8080)(route))
+  }
+
+  new WebWeaver(Map("httpServer" -> httpServer),
+    List(ModuleSpec("httpServer"))
+  )
 
 }
